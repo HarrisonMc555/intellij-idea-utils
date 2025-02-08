c3b551bc88157b59e1ef2d7744b9767ff72d91d7
GitHub Action <action@github.com>
2025-02-08 14:26:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/harrisonmc555/intellijideautils/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/harrisonmc555/intellijideautils/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.harrisonmc555.intellijideautils.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.harrisonmc555.intellijideautils.MyBundle
+import com.github.harrisonmc555.intellijideautils.services.MyProjectService
 import javax.swing.JButton
 
 
